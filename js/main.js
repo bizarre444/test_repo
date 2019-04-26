87062afc3108d7ef873fb1fc28859896f7b6884c
Olya <bizarre444@users.noreply.github.com>
2019-04-26 16:33:05 +0300

    without media

--- js/main.js
+++ js/main.js
@@ -68,3 +68,18 @@ function onButtonClick(e) {
         }
     };
 }
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href')
+    
+    document.querySelector('' + blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
